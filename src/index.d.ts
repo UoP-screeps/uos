6824cee89b4eb8df021b3dce5c17c9fe8ce6325a
cookiesjuice <hy140866@gmail.com>
2020-10-30 15:20:46 +0800

    Updated type definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,8 @@
 type Optional<T> = T | undefined;
 type Nullable<T> = T | undefined | null;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyArgs = any[];
 
 interface Memory {
     id?: number;
